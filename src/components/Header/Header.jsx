bd9e61623c806c178861f73b3b386d3060c8b2ac
k-ishida-niku <nikukaiman11@gmail.com>
2026-07-16 17:43:20 +0900

    たかがカルーセルされどカルーセル自信なくすねこれ

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,40 @@
 import style from "./Header.module.scss";
 import logo from "../../assets/img/global/logo.svg";
+import headerBart from "../../assets/img/global/header-bar.svg";
 export function Header() {
   return (
     <header className={style.header}>
-      <div className={style.logo}>
-        <a href="">
-          <img src={logo} alt="" />
-        </a>
+      <div className={style.headerBar}>
+        <ul className={style.hBarImg}>
+          {Array.from({ length: 10 }, (_, i) => {
+            return (
+              <li>
+                <img src={headerBart} alt="" />
+              </li>
+            );
+          })}
+        </ul>
+        <ul className={style.hBarImg}>
+          {Array.from({ length: 10 }, (_, i) => {
+            return (
+              <li>
+                <img src={headerBart} alt="" />
+              </li>
+            );
+          })}
+        </ul>
       </div>
-      <nav className={style.globalNav}></nav>
-      <button type="button" className={`${style.bBtn} f-dela`}>
-        MENU
-      </button>
+      <div className={style.headerInner}>
+        <div className={style.logo}>
+          <a href="">
+            <img src={logo} alt="" />
+          </a>
+        </div>
+        <nav className={style.globalNav}></nav>
+        <button type="button" className={`${style.bBtn} f-dela`}>
+          MENU
+        </button>
+      </div>
     </header>
   );
 }
